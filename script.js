b7e15651d0f8aa3b04bd927f923234957f4d08ee
Long Nguyen <nguye3lg@mail.uc.edu>
2025-04-15 21:52:15 -0400

    fixed FetchAPI for Render

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function sendPrompt() {
     responseBox.textContent = "";
   
     try {
-      const res = await fetch("http://127.0.0.1:8000/generate", {
+      const res = await fetch("https://mentor-ai-backend-skn0.onrender.com/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt })
